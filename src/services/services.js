fdd06a6ee045e80a94d5d6be716ba7d80d5eaa02
Oleksii Horban <lehagorban93@gmail.com>
2022-09-12 16:46:10 +0300

    commit

--- src/services/services.js
+++ src/services/services.js
@@ -18,18 +18,3 @@ export const removeContacts = async id => {
   const { data: result } = await instance.delete(`/${id}`);
   return result;
 };
-
-// const axios = require('axios').default;
-
-// export async function getContacts() {
-//   try {
-//     const response = await axios.get(
-//       `https://631c93a44fa7d3264cb1b965.mockapi.io/api/contacts`
-//     );
-//     const data = response.data;
-//     console.log(data);
-//     return data;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
